003d27c3f3f43aebf7a50a74df03263980ee40e3
huangyong <756655135@qq.com>
2020-07-28 16:06:12 +0800

    kotlin getter and setter

--- src/main/kotlin/hy/com/Child.kt
+++ src/main/kotlin/hy/com/Child.kt
@@ -5,13 +5,29 @@ package hy.com
  * description :
  */
 class Child(name: String, age: Int) : Father(name, age) {
+    /**
+     * kotlin中的Getter和setter定义好后，外部类不需要手动去调用，直接通过属性为其设值即可。
+     * child.firstProperty = "this is new first property"
+     * println(child)
+     */
     var firstProperty = "the first property is $name".also(::println)
-        get() = field
+        get() = field.toUpperCase()
         set(value) {
+            field = value
         }
 
+    /**
+     * companion object里面的方法或者成员可以直接通过类名来访问，相当于java中的public static成员
+     */
     companion object {
         fun haha(): String = "this is child fun"
+    }
+
+    /**
+     * 成员函数与扩展函数
+     */
+    fun function() {
+        println("this is child class method")
     }
 
     override fun xixi(): String = "this is child fun xixi"
@@ -21,4 +37,12 @@ class Child(name: String, age: Int) : Father(name, age) {
             Child.haha()
         }
     }
+
+    override fun toString(): String {
+        return "my firstProperty is $firstProperty"
+    }
+}
+
+fun Child.function(i: Int) {
+    println("this is child class extension method")
 }
--- src/main/kotlin/hy/com/Main.kt
+++ src/main/kotlin/hy/com/Main.kt
@@ -4,9 +4,16 @@ fun main(args: Array<String>) {
     val child = Child("zhang san", 12)
     println(Child.haha())
 
+    child.firstProperty = "this is new first property"
+    println(child)
+    child.function()
+
 //    foo()
 }
 
+/**
+ * 通过@go_on的形式，可以跳过循环当前的执行逻辑，继续执行下一逻辑
+ */
 fun foo() {
     listOf(1, 2, 3, 4, 5).forEach go_on@{
         if (it == 3)
